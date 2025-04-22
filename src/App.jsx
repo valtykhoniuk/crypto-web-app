741ce64163a124bd30cd8afe3557bd096ec7f16d
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-04-22 20:07:21 +0200

    slider: statistics, list added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-import { DatePicker } from "antd";
+import { Layout } from "antd";
+import AppHeader from "./components/layout/AppHeader";
+import AppSlider from "./components/layout/AppSlider";
+import AppContent from "./components/layout/AppContent";
+const { Header, Footer, Sider, Content } = Layout;
 
 export default function App() {
-  return <DatePicker />;
+  return (
+    <Layout>
+      <AppHeader />
+      <Layout>
+        <AppSlider />
+        <AppContent />
+      </Layout>
+    </Layout>
+  );
 }
