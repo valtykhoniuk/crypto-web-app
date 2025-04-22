491da7a3466b17636b2684fac427962a17a6e602
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-04-22 21:12:34 +0200

    slider finished

--- src/components/layout/AppSlider.jsx
+++ src/components/layout/AppSlider.jsx
@@ -1,53 +1,93 @@
-import { Layout, Card, Statistic, List, Typography } from "antd";
+import { Layout, Card, Statistic, List, Typography, Spin, Tag } from "antd";
 import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
+import { useEffect } from "react";
+import { useState } from "react";
+import { fetchAssets, fakeFetchCrypto } from "../../api";
+import { percentDifference, capitalize } from "../../utils";
 
 const siderStyle = {
-  textAlign: "center",
-  lineHeight: "120px",
-  color: "#fff",
   padding: "1rem",
 };
 
-const data = [
-  "Racing car sprays burning fuel into crowd.",
-  "Japanese princess to wed commoner.",
-  "Australian walks 100km after outback crash.",
-  "Man charged over missing wedding girl.",
-  "Los Angeles battles huge wildfires.",
-];
-
 export default function AppSlider() {
+  const [loading, setLoading] = useState(false);
+  const [crypto, setCrypto] = useState([]);
+  const [assets, setAssets] = useState([]);
+
+  useEffect(() => {
+    async function preload() {
+      setLoading(true);
+      const { result } = await fakeFetchCrypto();
+      const assets = await fetchAssets();
+
+      setAssets(
+        assets.map((asset) => {
+          const coin = result.find((c) => c.id === asset.id);
+          return {
+            grow: asset.price < coin.price,
+            growPercent: percentDifference(asset.price, coin.price),
+            totalAmount: asset.amount * coin.price,
+            totalProfit: asset.amount * coin.price - asset.amount * asset.price,
+            ...asset,
+          };
+        })
+      );
+      setCrypto(result);
+      setLoading(false);
+    }
+
+    preload();
+  }, []);
+
+  if (loading) {
+    <Spin fullscreen />;
+  }
+
   return (
     <Layout.Sider width="25%" style={siderStyle}>
-      <Card style={{ marginBottom: "1rem" }}>
-        <Statistic
-          title="Active"
-          value={11.28}
-          precision={2}
-          valueStyle={{ color: "#3f8600" }}
-          prefix={<ArrowUpOutlined />}
-          suffix="%"
-        />
-        <List
-          size="small"
-          dataSource={data}
-          renderItem={(item) => (
-            <List.Item>
-              <Typography.Text mark>[ITEM]</Typography.Text> {item}
-            </List.Item>
-          )}
-        />
-      </Card>
-      <Card style={{ marginBottom: "1rem" }}>
-        <Statistic
-          title="Idle"
-          value={9.3}
-          precision={2}
-          valueStyle={{ color: "#cf1322" }}
-          prefix={<ArrowDownOutlined />}
-          suffix="%"
-        />
-      </Card>
+      {assets.map((asset) => (
+        <Card key={asset.id} style={{ marginBottom: "1rem" }}>
+          <Statistic
+            title={capitalize(asset.id)}
+            value={asset.totalAmount}
+            precision={2}
+            valueStyle={{ color: asset.grow ? "#3f8600" : "#cf1322" }}
+            prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+            suffix="$"
+          />
+          <List
+            size="small"
+            dataSource={[
+              {
+                title: "Total profit",
+                value: asset.totalProfit,
+                withTag: true,
+              },
+              { title: "Asset amount", value: asset.amount, isPlain: true },
+              //   { title: "Difference", value: asset.growPercent },
+            ]}
+            renderItem={(item) => (
+              <List.Item>
+                <span>{item.title}</span>
+                <span>
+                  {item.withTag && (
+                    <Tag color={asset.grow ? "green" : "red"}>
+                      {asset.growPercent}%
+                    </Tag>
+                  )}
+                  {item.isPlain && item.value}
+
+                  {!item.isPlain && (
+                    <Typography.Text type={asset.grow ? "success" : "danger"}>
+                      {item.value.toFixed(2)}$
+                    </Typography.Text>
+                  )}
+                </span>
+              </List.Item>
+            )}
+          />
+        </Card>
+      ))}
     </Layout.Sider>
   );
 }
